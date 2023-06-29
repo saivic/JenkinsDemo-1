7e93a50cbd4975d605698a892f4d9079d35ccde0
saivi <saivichatterjee97@gmail.com>
2023-06-29 16:45:18 +0530

    Initial commit

---
+++ src/main/java/com/cognizant/skygrade/JenkinsDemo2Application.java
@@ -0,0 +1,13 @@
+package com.cognizant.skygrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognizant/skygrade/JenkinsDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.skygrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
